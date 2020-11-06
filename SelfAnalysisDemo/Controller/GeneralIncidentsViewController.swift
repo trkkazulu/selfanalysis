2e42b96e0f21f4bb7a1edb07adde712f8fb67980
trkkazulu2 <jairrohm@gmail.com>
2020-11-06 18:02:07 -0500

    I've added the class for the first list with buttons to advance the instructions and return to the TOC

---
+++ SelfAnalysisDemo/Controller/GeneralIncidentsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GeneralIncidentsViewController.swift
+//  SelfAnalysisDemo
+//
+//  Created by Jair-Rohm Wells on 11/6/20.
+//
+
+import UIKit
+
+class GeneralIncidentsViewController: UIViewController {
+    
+    @IBOutlet weak var instructionLbl: UILabel!
+    
+    @IBOutlet weak var percepticLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func nextInstructionBtn(_ sender: Any) {
+    }
+    
+    @IBAction func backTOCBtn(_ sender: Any) {
+    }
+}
